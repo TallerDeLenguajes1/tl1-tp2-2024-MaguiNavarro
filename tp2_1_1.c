c7fc983cce63ba7e31d496d8f0cd1f3d7853a10b
“Magali <maguinavarro07@gmail.com>
2024-04-09 15:18:46 -0300

    punto 2.a terminado

---
+++ tp2_1_1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+// codigo a completar
+#define N 20
+int main()
+{
+   srand(time(NULL));
+    int i;
+    double vt[N];
+
+for ( i = 0; i < N; i++)
+{
+
+    vt[i] = 1 + rand() % 100;
+    printf("%.2f\n" , vt[i]);
+}
+
+
+
+    return 0;
+}
+
